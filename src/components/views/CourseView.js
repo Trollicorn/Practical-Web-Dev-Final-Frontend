d861f4f29f3238cd7bec74ca6b3dcf7708643036
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2022-05-24 15:38:34 -0400

    can go to instructor view from course page

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const CourseView = (props) => {
   const { course } = props;
   return (
     <div>
       <h1>{course.title}</h1>
-      {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>staff</h3>}
+      {course.instructor ? 
+        <Link to={`/instructor/${course.instructorId}`}>
+          <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>
+        </Link>
+        : <h3>staff</h3>
+      }
       <h4>Timeslot: {course.timeslot}</h4>
       <h4>Location: {course.location}</h4>
     </div>
