d22a6c0f2e445de9605683a23bdf98421d79b6c4
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2022-05-24 15:31:56 -0400

    added info showing on course view + location input for new course

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -4,6 +4,8 @@ const CourseView = (props) => {
     <div>
       <h1>{course.title}</h1>
       {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>staff</h3>}
+      <h4>Timeslot: {course.timeslot}</h4>
+      <h4>Location: {course.location}</h4>
     </div>
   );
 
